95ceb8aad9d5c2d037cc3a045e252ce5ba69c66d
Atharva2000 <54733112+Atharva2000@users.noreply.github.com>
2020-02-28 13:45:11 +0100

    Add files via upload

--- binary.py
+++ binary.py
@@ -10,6 +10,8 @@ import numpy as np
 from datetime import datetime
 
 cam = cv2.VideoCapture(0)       # Captures the video from webcam
+ret, frame = cam.read() 
+r = cv2.selectROI(frame)    
 
 cv2.namedWindow("test1")
 
@@ -21,12 +23,14 @@ val = 0
 sat = 0
 while True:
     ret, frame = cam.read()     # reads the images from video frames
+    imCrop = frame[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]  # Crop image
+    hsv_frame = cv2.cvtColor(imCrop, cv2.COLOR_BGR2HSV)
     
     # Put current DateTime on each frame
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(frame,str(datetime.now()),(10,30), font, 1,(0,0,255),2,cv2.LINE_AA)
     # Display the image
-    cv2.imshow('a',frame)
+    cv2.imshow('a',imCrop)
     
     if not ret:
         break
@@ -43,8 +47,8 @@ while True:
         print("{} written!".format(img_name))
         img_counter += 1
     
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    img_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    gray_frame = cv2.cvtColor(imCrop, cv2.COLOR_BGR2GRAY)
+    img_hsv = cv2.cvtColor(imCrop, cv2.COLOR_BGR2HSV)
     ret,thresh1 = cv2.threshold(gray_frame,200,255,cv2.THRESH_BINARY)  
     
     contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)   # returns boundary points of the contour
@@ -62,7 +66,7 @@ while True:
         crop_img = frame[y:y+h, x:x+w]
         
         if (cv2.contourArea(contour) > 100):
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)    # draws a rectangle around area of interest in the frame
+            cv2.rectangle(frame,(r[0]+x,r[1]+y),(r[0]+x+w,r[1]+y+h),(0,0,255),2)    # draws a rectangle around area of interest in the frame
         
             a = [img_hsv[l,m,0] for [m,l] in contours[ind][:,0,:]]                                   # list of hue values of the boundary points of the contour
             s = [img_hsv[l,m,1] for [m,l] in contours[ind][:,0,:]]                                   # returns saturation value of the boundary points 
@@ -75,13 +79,23 @@ while True:
             hue=np.mean(a)                                                        # average hue values of the boundary points
             sat=np.mean(s)                                                        # average saturation values of the boundary points
             val=np.mean(v)
-
-        
+            if(sat<30):
+                print("LED Off")
+            else:
+                print("LED On")
+                if (hue<115 and hue>95):
+                    print("BLUE")
+                elif (hue<75 and hue>63):
+                    print("YELLOW")
+                elif (hue<60 and hue>48):
+                    print("WHITE")
+                
+    cv2.rectangle(frame,(r[0],r[1]),(r[0]+r[2],r[1]+r[3]),(0,0,255),2)
     print(hue,sat,val)
     cv2.imshow('test2',thresh1)
     cv2.imshow("test1", frame)
     
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(10) & 0xFF == ord('q'):
         break
     
 cam.release()
